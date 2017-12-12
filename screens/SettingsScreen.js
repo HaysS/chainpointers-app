328a571c4fa7f6e1bc4aa25241806d2312c46f63
Hays Stanford <stanfordhays@gmail.com>
2017-12-12 13:38:12 -0800

    Made basic changes removing pointless template content in screens.

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { 
+	View, 
+	Text, 
+ 	StyleSheet, 
+} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    title: 'Settings',
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return(
+    	<View style={styles.container}>
+    		<Text>Settings Screen</Text>
+  		</View>
+	);
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
