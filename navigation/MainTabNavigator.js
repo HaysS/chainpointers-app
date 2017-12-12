ffcb2e75a39eeedd7a07ccf1698bf17599916961
Hays Stanford <stanfordhays@gmail.com>
2017-12-12 13:42:38 -0800

    Removed links screen. Now just home and settings.

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -6,16 +6,12 @@ import { TabNavigator, TabBarBottom } from 'react-navigation';
 import Colors from '../constants/Colors';
 
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 export default TabNavigator(
   {
     Home: {
       screen: HomeScreen,
-    },
-    Links: {
-      screen: LinksScreen,
     },
     Settings: {
       screen: SettingsScreen,
@@ -28,12 +24,6 @@ export default TabNavigator(
         let iconName;
         switch (routeName) {
           case 'Home':
-            iconName =
-              Platform.OS === 'ios'
-                ? `ios-information-circle${focused ? '' : '-outline'}`
-                : 'md-information-circle';
-            break;
-          case 'Links':
             iconName = Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link';
             break;
           case 'Settings':
